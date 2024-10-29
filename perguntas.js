467d346c428cd4bc27dd4d230ce4b5bbe2a95628
Neyjotaerre <141829992+Neyjotaerre@users.noreply.github.com>
2024-10-29 08:45:59 -0300

    hy

---
+++ perguntas.js
@@ -0,0 +1,45 @@
+criaCartao(
+    'programaçao',
+    'O que é javascript?',
+    'o javascript é uma linguagem de programaçao'
+)
+criaCartao(
+    'matematica',
+    'O que é angulo?',
+    'Ângulo é a reunião de dois segmentos de reta orientados a partir de um ponto comum.'
+)
+criaCartao(
+    'Português',
+    'Qual o objetivo de um texto argumentativo?',
+    'Um texto argumentativo tem como objetivo convencer alguém das nossas ideias.'
+)
+criaCartao(
+    'Biologia',
+    'Quem propos a teoria da seleçao natural',
+    'Charles Darwin'
+)
+criaCartao(
+    'Biotecnologia',
+    'O que sao celulas',
+    'A célula é a unidade estrutural e funcional dos seres vivos'
+)
+criaCartao(
+    'Robotica',
+    'O que é push botom',
+    'é um tipo de interruptor simples utilizado para acionar ou interromper um circuito elétrico.'
+)
+criaCartao(
+    'Projeto de vida',
+    'O que é Empatia',
+    'ce colocar no lugar do proximo'
+)
+criaCartao(
+    '',
+    '',
+    ''
+)
+criaCartao(
+    '',
+    '',
+    ''
+)
